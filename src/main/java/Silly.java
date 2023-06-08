b01c16ef1e7ac7ae42f3cf70af20de2150b3801d
Skhaki18 <samir.khaki@mail.utoronto.ca>
2023-06-08 03:35:11 -0700

    Completed the tasks for Silly.java, all unit tests pass.

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -80,7 +80,16 @@ public class Silly implements Comparable<Silly>{
      *       Make sure you document this method!
      */
 
-
+    /**
+     * Creates a new Silly object.
+     * This constructor takes in two strings as arguments.
+     *
+     * @param s1 is a string for the prefix in Silly instance's name.
+     * @param s2 is a string for the suffix in Silly instance's name.
+     */
+    public Silly(String s1, String s2) {
+        this.name = s1+s2;
+    }
 
 
 
@@ -116,7 +125,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0,1,2,3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -134,6 +143,7 @@ public class Silly implements Comparable<Silly>{
     @Override
     public String toString(){
         // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
@@ -161,6 +171,12 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+        if (this.name.equals(other.name)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     /**
@@ -194,6 +210,12 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+        if (this.name.length() > other.name.length()){return 1;}
+        else if (this.name.length() < other.name.length()) {return -1;}
+        else{return 0;}
+
+        }
+
     }
 
     /*
@@ -205,5 +227,5 @@ public class Silly implements Comparable<Silly>{
      *                If the tests don't pass, look at the results and revise
      *                accordingly.
      */
-}
+//}
 
